cc55b78fb52ca343b87f35d8aa74a69ab9a55c61
Segaud Kevin <segaud.kevin@gmail.com>
2014-07-09 20:21:28 +0200

    add desktop-capture sample

--- camera-capture/web/camera_capture.dart
+++ camera-capture/web/camera_capture.dart
@@ -1,7 +1,5 @@
 import 'dart:html';
 
-import "package:chrome/chrome_app.dart" as chrome;
-
 void main() {
   document.querySelector("button").onClick.listen((MouseEvent mouse_event) {
     window.navigator.getUserMedia(audio: true, video: true).then((MediaStream media_stream) {
---
+++ desktop-capture/web/desktop_capture.dart
@@ -0,0 +1,59 @@
+
+import 'dart:html';
+import "dart:convert";
+
+import 'package:chrome/chrome_ext.dart' as chrome;
+
+var desktop_sharing = false;
+var local_stream = null;
+
+void onAcceptApproved(desktop_id) {
+  print(desktop_id.runtimeType);
+  if (desktop_id == null) {
+    print('Desktop Capture access rejected.');
+    return;
+  }
+  
+  desktop_sharing = true;
+  document.querySelector('button').innerHtml = "Disable Capture";
+  print("Desktop sharing started.. desktop_id: $desktop_id");
+  
+  window.navigator.getUserMedia(audio: false, video: { "mandatory": {
+      "chromeMediaSource": 'desktop',
+      "chromeMediaSourceId": desktop_id,
+      "minWidth": 1280,
+      "maxWidth": 1280,
+      "minHeight": 720,
+      "maxHeight": 720
+    }
+  }).then((MediaStream media_stream) {
+    local_stream = media_stream;
+    VideoElement video = document.querySelector('video');
+    video.src = Url.createObjectUrl(media_stream);
+    media_stream.onEnded.listen((Event event) {
+      if (desktop_sharing) {
+        toggle();
+      }
+    });
+    
+  }).catchError((e) => print('getUserMediaError: $e'));
+}
+
+void toggle() {
+  if (!desktop_sharing) 
+    chrome.desktopCapture.chooseDesktopMedia(['screen', 'window'], onAcceptApproved);
+  else {
+    desktop_sharing = false;
+    if (local_stream != null) 
+      local_stream.stop();
+    local_stream = null;
+    document.querySelector('button').innerHtml = "Enable Capture";
+    print('Desktop sharing stopped...');
+  }
+}
+
+void main() {
+  document.querySelector("button").onClick.listen((event) {
+    toggle();
+  });
+}
